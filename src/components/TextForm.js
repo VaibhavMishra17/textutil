b37eb86f092eecfa8f8d8667819af85eb3a5f998
Vaibhav Mishra <vmishra175@gmail.com>
2021-08-30 13:13:18 +0530

    added more  buttons

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -8,8 +8,12 @@ export default function TextForm(props) {
   };
 
   const handleLoClick = () => {
-    // console.log("Uppercase was clicked: " +  text);
     let newText = text.toLowerCase();
+    setText(newText);
+  };
+
+  const handleClearClick = () => {
+    let newText = "";
     setText(newText);
   };
 
@@ -39,6 +43,9 @@ export default function TextForm(props) {
         </button>
         <button className="btn btn-primary mx-1" onClick={handleLoClick}>
           Convert to Lowercase
+        </button>
+        <button className="btn btn-primary mx-1" onClick={handleClearClick}>
+          Clear Text
         </button>
       </div>
       <div className="container my-3">
