aca0853326120116a4e65cfd36340eeb8db91e0f
Vaibhav Mishra <vmishra175@gmail.com>
2021-08-30 13:16:09 +0530

    added more buttons and functions

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -22,6 +22,19 @@ export default function TextForm(props) {
     setText(event.target.value);
   };
 
+  const handleCopy = () => {
+    console.log("I am copy");
+    var text = document.getElementById("myBox");
+    text.select();
+    text.setSelectionRange(0, 9999);
+    navigator.clipboard.writeText(text.value);
+  };
+
+  const handleExtraSpaces = () => {
+    let newText = text.split(/[ ]+/);
+    setText(newText.join(" "));
+  };
+
   const [text, setText] = useState("");
   // text = "new text"; // Wrong way to change the state
   // setText("new text"); // Correct way to change the state
@@ -46,6 +59,12 @@ export default function TextForm(props) {
         </button>
         <button className="btn btn-primary mx-1" onClick={handleClearClick}>
           Clear Text
+        </button>
+        <button className="btn btn-primary mx-1" onClick={handleCopy}>
+          Copy Text
+        </button>
+        <button className="btn btn-primary mx-1" onClick={handleExtraSpaces}>
+          Remove Extra Spaces
         </button>
       </div>
       <div className="container my-3">
